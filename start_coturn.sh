395ee706848925c29d32dcf84bcd53e453632b1c
Aitouakli Hichem <alterhichem99@gmail.com>
2022-05-19 17:25:11 +0100

    nodejs server

--- start_coturn.sh
+++ start_coturn.sh
@@ -12,3 +12,7 @@ echo password : $(echo -n $username | openssl dgst -binary -sha1 -hmac $secret |
 echo "Starting TURN/STUN server............."
 
 turnserver -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_SECRET}" --realm=${TURN_REALM}  -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} ${TURN_EXTRA}
+
+nodejs -v
+
+node index.js
