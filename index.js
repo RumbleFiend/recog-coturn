38861ce77ba1ef9d41d2918a86e8517055e3829c
Aitouakli Hichem <alterhichem99@gmail.com>
2022-05-19 17:30:59 +0100

    changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 app.listen(8080, () => {
   console.log("Server running on port 3000");
 });
-
+//api
 app.get("/", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
