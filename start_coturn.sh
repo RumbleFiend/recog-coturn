2cd20d2c0d62130b1eeced40b39c381b692889d2
Aitouakli Hichem <alterhichem99@gmail.com>
2022-05-20 17:41:30 +0100

    changed command order

--- start_coturn.sh
+++ start_coturn.sh
@@ -9,9 +9,10 @@ username=$(( $time + $expiry )) &&\
 echo username:$username && \
 echo password : $(echo -n $username | openssl dgst -binary -sha1 -hmac $secret | openssl base64)
 
-echo "Starting TURN/STUN server............."
-
-turnserver -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_SECRET}" --realm=${TURN_REALM}  -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} ${TURN_EXTRA}
 
 echo "Starting Nodejs Server............."
 node index.js
+
+echo "Starting TURN/STUN server............."
+
+turnserver -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_SECRET}" --realm=${TURN_REALM}  -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} ${TURN_EXTRA}
